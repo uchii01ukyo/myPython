0c0160259224cdf7b76bd28a44c81dd5d1a8c0c8
Uchii Ukyo <uchii01ukyo@gmail.com>
2022-10-02 16:08:26 +0900

    no message

--- multi_thread.py
+++ multi_thread.py
@@ -1,6 +1,8 @@
+# coding: UTF-8
 import threading
 import time
 import concurrent.futures
+import multiprocessing
 
 # How to use class
 def main_class():
@@ -9,20 +11,6 @@ def main_class():
     t1.start()
     t1.join()
     print("end")
-
-# How to use instance
-def main_instance():
-    t1 = threading.Thread(target=foo, name="foo", args=(5,))
-    t1.setDaemon(True)
-    t1.start()
-    t1.join()
-    print("end")
-
-# How to use Threadpool
-def main_threadpool():
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-    executor.submit(func1)
-    executor.submit(func2)
 
 class TestThread(threading.Thread):
     def __init__(self):
@@ -33,22 +21,58 @@ class TestThread(threading.Thread):
             time.sleep(1)
             print("sub thread : " + str(i))
 
+# How to use instance
+def main_instance():
+    t1 = threading.Thread(target=foo, name="foo", args=(5,))
+    t1.setDaemon(True)
+    t1.start()
+    t1.join()
+    print("end")
+
 def foo(n):
     for i in range(n):
         print("sub thread : " + str(i))
+
+# How to use Threadpool
+def main_threadpool():
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    executor.submit(func1)
+    executor.submit(func2)
 
 def func1():
     while True:
         print("func1")
         time.sleep(1)
 
-
 def func2():
     while True:
         print("func2")
         time.sleep(1)
 
+
+# How to use multiprocess
+def main_multiprocess():
+    p = multiprocessing.Process(target=func_2, args=(10,))
+    p.start()
+    func_1(10)
+
+def func_1(num):
+    print('メインプロセスStart')
+    for i in range(num):
+        print('メインプロセス:', i)
+        time.sleep(1)
+    print('メインプロセスEnd')
+
+def func_2(num):
+    print('サブプロセスStart')
+    for i in range(num):
+        print('サブプロセス:', i)
+        time.sleep(0.5)
+    print('サブプロセスEnd')
+
+
 if __name__ == '__main__':
-    main_class()
+    #main_class()
     #main_instance()
     #main_threadpool()
+    main_multiprocess()
